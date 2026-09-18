40d42112175858edb94cd95bca21872aec088156
benyr <benyrooz00@gmail.com>
2026-09-18 12:40:11 +0200

    Clear navigation backstack when returning Home

--- app/src/main/java/com/benyamin/weathertrack/DetailsFragment.kt
+++ app/src/main/java/com/benyamin/weathertrack/DetailsFragment.kt
@@ -1,5 +1,18 @@
 package com.benyamin.weathertrack
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class DetailsFragment : Fragment(R.layout.fragment_details)
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.buttonBackHome).setOnClickListener {
+            findNavController().popBackStack(R.id.homeFragment, false)
+        }
+    }
+}
